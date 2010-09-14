09cf855705dffcf91d77bdf852487be86c384507
gegismero <gegismero@eaa6a7b1-3fd6-b1f1-36e1-1d5117f4dc80>
2010-09-14 21:51:58 +0000

    Cliente para ejecutar la parte 2

--- trunk/eclipse_proj/TSI_PracticoJPA/src/dao/CacheManager.java
+++ trunk/eclipse_proj/TSI_PracticoJPA/src/dao/CacheManager.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.rmi.RemoteException;
+import java.util.Set;
 
 import javax.ejb.EJBMetaData;
 import javax.ejb.Handle;
@@ -10,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import bean.Cache;
+import bean.Webservice;
 import client.ICacheManager;
 
 public class CacheManager implements ICacheManager {
@@ -37,9 +39,30 @@ public class CacheManager implements ICacheManager {
 
 	}
 
-	public Cache create(int ID, String params, String result) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+	public Cache create(String params, String result, int idws) throws RemoteException {
+		Cache c = new Cache();
+		
+		Webservice ws = em.find(Webservice.class, idws);
+		
+		if(ws == null)
+			throw new RemoteException("No exsite el Webservice con idws" + idws);
+		
+		//El ID se asigna automaticamente. Ver bean.Cache por mas informacion
+		
+		c.setParams(params);
+		c.setResult(result);
+		c.setWebservice(ws);
+		
+		em.persist(c);
+		
+		//Agregamos a c a la lista de Caches de ws
+		Set<Cache> sC = ws.getCaches();
+		sC.add(c);
+		ws.setCaches(sC);
+		
+		em.persist(ws);
+		
+		return c;
 	}
 
 }
