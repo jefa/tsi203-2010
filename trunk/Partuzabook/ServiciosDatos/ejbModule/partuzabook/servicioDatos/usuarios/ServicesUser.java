76f5cd62aff029cb47e676382e5d04f31ba2c038
aenavcam@gmail.com <aenavcam@gmail.com@eaa6a7b1-3fd6-b1f1-36e1-1d5117f4dc80>
2010-11-19 00:18:59 +0000

    Parche satanico para obtener la imagen de Facebook del usuario

--- trunk/Partuzabook/ServiciosDatos/ejbModule/partuzabook/servicioDatos/usuarios/ServicesUser.java
+++ trunk/Partuzabook/ServiciosDatos/ejbModule/partuzabook/servicioDatos/usuarios/ServicesUser.java
@@ -1,5 +1,12 @@
 package partuzabook.servicioDatos.usuarios;
 
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -11,6 +18,7 @@ import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
+import javax.imageio.ImageIO;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
@@ -44,59 +52,64 @@ public class ServicesUser implements ServicesUserRemote {
 
 	private static final String DEFAULT_IMAGE = "profile/avatar_default.png";
 	private static final String AVATAR_PATH = "profile";
-	
-	
+
 	private NormalUserDAO nUserDao;
 	private AdminDAO adminDao;
 	private FileSystemLocal fileSystem;
 	private TagForUserDAO tagForUserDao;
 	private NotificationDAO notifDao;
-		
-    /**
-     * Default constructor. 
-     */
-    public ServicesUser() {
 
-    }
-    
-    @PostConstruct
-    public void postConstruct() {
-        try {
-        	Properties properties = new Properties();
-	        properties.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
-	        properties.put("java.naming.factory.url.pkgs", "org.jboss.naming rg.jnp.interfaces");
-	        properties.put("java.naming.provider.url", "jnp://localhost:1099");
-	        Context ctx = new InitialContext(properties);
-	        adminDao = (AdminDAO) ctx.lookup("AdminDAOBean/local");
-	        nUserDao = (NormalUserDAO) ctx.lookup("NormalUserDAOBean/local"); 
-    		fileSystem = (FileSystemLocal) ctx.lookup("FileSystem/local");
-    		tagForUserDao = (TagForUserDAO) ctx.lookup("TagForUserDAOBean/local");    		
-	        notifDao = (NotificationDAO) ctx.lookup("NotificationDAOBean/local");
-		}
-        catch (Exception e) {
+	/**
+	 * Default constructor.
+	 */
+	public ServicesUser() {
+
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		try {
+			Properties properties = new Properties();
+			properties.put("java.naming.factory.initial",
+					"org.jnp.interfaces.NamingContextFactory");
+			properties.put("java.naming.factory.url.pkgs",
+					"org.jboss.naming rg.jnp.interfaces");
+			properties.put("java.naming.provider.url", "jnp://localhost:1099");
+			Context ctx = new InitialContext(properties);
+			adminDao = (AdminDAO) ctx.lookup("AdminDAOBean/local");
+			nUserDao = (NormalUserDAO) ctx.lookup("NormalUserDAOBean/local");
+			fileSystem = (FileSystemLocal) ctx.lookup("FileSystem/local");
+			tagForUserDao = (TagForUserDAO) ctx
+					.lookup("TagForUserDAOBean/local");
+			notifDao = (NotificationDAO) ctx
+					.lookup("NotificationDAOBean/local");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-    
-    @PreDestroy
-    public void preDestroy() {
-    	adminDao = null;
-    	nUserDao = null;
-    	fileSystem = null;
-    	tagForUserDao = null;
-    	notifDao = null;
-    }
+	}
 
-	public DatatypeUser createNormalUser(String username, String password, String mail, String name) throws UserAlreadyExistsException{
+	@PreDestroy
+	public void preDestroy() {
+		adminDao = null;
+		nUserDao = null;
+		fileSystem = null;
+		tagForUserDao = null;
+		notifDao = null;
+	}
+
+	public DatatypeUser createNormalUser(String username, String password,
+			String mail, String name) throws UserAlreadyExistsException {
 		if (existsAdminUser(username) || existsNormalUser(username)) {
 			throw new UserAlreadyExistsException();
 		}
 		String pass = "";
 		try {
 			pass = CryptUtils.encript(password);
-		} catch(NoSuchAlgorithmException e) { 
-			//Hacemos la excepcion transparente al usuario
-			System.out.println("Error al crear un usuario, fallo la creación del algoritmo MD5 para el usuario " + username + " con password" + password);
+		} catch (NoSuchAlgorithmException e) {
+			// Hacemos la excepcion transparente al usuario
+			System.out
+					.println("Error al crear un usuario, fallo la creaciï¿½n del algoritmo MD5 para el usuario "
+							+ username + " con password" + password);
 			throw new UserAlreadyExistsException();
 		}
 		NormalUser newUser = new NormalUser();
@@ -108,11 +121,13 @@ public class ServicesUser implements ServicesUserRemote {
 		newUser.setRegDate(new Timestamp(new java.util.Date().getTime()));
 		newUser.setFacebookUser(false);
 		nUserDao.persist(newUser);
-		
-		return (DatatypeUser)new TranslatorUser().translate(newUser);
+
+		return (DatatypeUser) new TranslatorUser().translate(newUser);
 	}
-	
-	public DatatypeUser createNormalUser(String username, String password, String mail, String name,long facebookId) throws UserAlreadyExistsException{
+
+	public DatatypeUser createNormalUser(String username, String password,
+			String mail, String name, long facebookId)
+			throws UserAlreadyExistsException {
 		if (existsAdminUser(username) || existsNormalUser(username)) {
 			throw new UserAlreadyExistsException();
 		}
@@ -126,20 +141,23 @@ public class ServicesUser implements ServicesUserRemote {
 		newUser.setFacebookId(facebookId);
 		newUser.setFacebookUser(true);
 		nUserDao.persist(newUser);
-		
-		return (DatatypeUser)new TranslatorUser().translate(newUser);
+
+		return (DatatypeUser) new TranslatorUser().translate(newUser);
 	}
-	
-	public DatatypeUser createAdminUser(String username, String password, String mail, String name) throws UserAlreadyExistsException{
+
+	public DatatypeUser createAdminUser(String username, String password,
+			String mail, String name) throws UserAlreadyExistsException {
 		if (existsAdminUser(username) || existsNormalUser(username)) {
 			throw new UserAlreadyExistsException();
 		}
 		String pass = "";
 		try {
 			pass = CryptUtils.encript(password);
-		} catch(NoSuchAlgorithmException e) { 
-			//Hacemos la excepcion transparente al usuario
-			System.out.println("Error al crear un usuario, fallo la creación del algoritmo MD5 para el usuario " + username + " con password" + password);
+		} catch (NoSuchAlgorithmException e) {
+			// Hacemos la excepcion transparente al usuario
+			System.out
+					.println("Error al crear un usuario, fallo la creaciï¿½n del algoritmo MD5 para el usuario "
+							+ username + " con password" + password);
 			throw new UserAlreadyExistsException();
 		}
 		Admin newUser = new Admin();
@@ -149,97 +167,97 @@ public class ServicesUser implements ServicesUserRemote {
 		newUser.setName(name);
 		newUser.setImgPath("");
 		newUser.setRegDate(new Timestamp(new java.util.Date().getTime()));
-		
+
 		adminDao.persist(newUser);
-		
-		return (DatatypeUser)new TranslatorUser().translate(newUser);
+
+		return (DatatypeUser) new TranslatorUser().translate(newUser);
 	}
-	
-	public DatatypeUser updateNormalUser(String username, String password, String mail, String name) throws UserNotFoundException {
-		if(!existsNormalUser(username))
+
+	public DatatypeUser updateNormalUser(String username, String password,
+			String mail, String name) throws UserNotFoundException {
+		if (!existsNormalUser(username))
 			throw new UserNotFoundException();
-		
+
 		NormalUser nu = nUserDao.findByID(username);
 		nu.setPassword(password);
 		nu.setEmail(mail);
 		nu.setName(name);
 		nUserDao.persist(nu);
-		return (DatatypeUser)new TranslatorUser().translate(nu);
+		return (DatatypeUser) new TranslatorUser().translate(nu);
 	}
-	
+
 	public boolean existsNormalUser(String username) {
 		return nUserDao.findByID(username) != null;
 	}
-	
+
 	public boolean existsFacebookUser(long facebookid) {
 		return nUserDao.findByFacebookId(facebookid) != null;
 	}
 
-	
-	public String getName(String username){
+	public String getName(String username) {
 		String name = "";
 		NormalUser nUser = nUserDao.findByID(username);
 		if (nUser != null) {
 			name = nUser.getName();
-		} 
+		}
 		return name;
 	}
-	
-	
+
 	public List<Boolean> existsNormalUser(List<String> usernames) {
 		List<Boolean> res = new ArrayList<Boolean>();
-		for(ListIterator<String> it = usernames.listIterator(); it.hasNext(); ){
-			res.add(existsNormalUser(it.next()));				
-		} 
+		for (ListIterator<String> it = usernames.listIterator(); it.hasNext();) {
+			res.add(existsNormalUser(it.next()));
+		}
 		return res;
 	}
-	
+
 	private NormalUser getNormalUser(String username) {
 		NormalUser user = nUserDao.findByID(username);
 		if (user == null) {
 			throw new UserNotFoundException();
-		} 
+		}
 		return user;
 	}
-	
+
 	public NormalUser getNormalUserByFacebookId(long facebookId) {
 		NormalUser user = nUserDao.findByFacebookId(facebookId);
 		if (user == null) {
 			throw new UserNotFoundException();
-		} 
+		}
 		return user;
 	}
-	
+
 	public boolean existsAdminUser(String username) {
-		return adminDao.findByID(username) !=null;
+		return adminDao.findByID(username) != null;
 	}
-	
+
 	private Admin getAdminUser(String username) {
 		Admin user = adminDao.findByID(username);
 		if (user == null) {
 			throw new UserNotFoundException();
-		} 
+		}
 		return user;
 	}
-	
+
 	public DatatypeUser getUserForPublicProfile(String username) {
-		return (DatatypeUser)new TranslatorUser().translate(getNormalUser(username));
+		return (DatatypeUser) new TranslatorUser()
+				.translate(getNormalUser(username));
 	}
- 
-    public List<DatatypeEventSummary> getEventSummaryByUser(String user) {
-    	NormalUser nUser = getNormalUser(user);   	
-    	List<Event> ret = nUser.getMyEvents();
-    	return TranslatorCollection.translateEventSummary(ret);
-    }
+
+	public List<DatatypeEventSummary> getEventSummaryByUser(String user) {
+		NormalUser nUser = getNormalUser(user);
+		List<Event> ret = nUser.getMyEvents();
+		return TranslatorCollection.translateEventSummary(ret);
+	}
 
 	public String getNormalUserPassword(String username) {
 		return getNormalUser(username).getPassword();
 	}
-	
+
 	public String getNormalUserMailAddress(String username) {
 		return getNormalUser(username).getEmail();
 	}
-	
+
 	public String getAdminUserPassword(String username) {
 		return getAdminUser(username).getPassword();
 	}
@@ -248,21 +266,44 @@ public class ServicesUser implements ServicesUserRemote {
 		if (username.startsWith("__unr_"))
 			return fileSystem.readFile(DEFAULT_IMAGE, thumbnail);
 		NormalUser user = getNormalUser(username);
-		if(user.getImgPath() != null && !user.getImgPath().equals(""))
+		if (user.isFacebookUser()) {
+			URL url = null;
+			try {
+				url = new URL("http://graph.facebook.com/"	+ user.getFacebookId() + "/picture");
+				java.awt.Image image = ImageIO.read(url);
+
+				BufferedImage bi = new BufferedImage(image.getWidth(null),image.getHeight(null),BufferedImage.TYPE_INT_RGB);
+				Graphics g = bi.getGraphics();
+				g.drawImage(image,0,0,null);
+				g.dispose();
+
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ImageIO.write(bi, "jpeg", baos);
+				byte[] bytesOut = baos.toByteArray();
+
+				return bytesOut;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return fileSystem.readFile(DEFAULT_IMAGE, thumbnail);
+			}
+
+		}
+
+		if (user.getImgPath() != null && !user.getImgPath().equals(""))
 			return fileSystem.readFile(user.getImgPath(), thumbnail);
 		else
 			return fileSystem.readFile(DEFAULT_IMAGE, thumbnail);
 	}
 
-	public List<DatatypeUser> findAllNormalUsers() {		
+	public List<DatatypeUser> findAllNormalUsers() {
 		return TranslatorCollection.translateNormalUser(nUserDao.findAll());
 	}
-	
+
 	private List<String> getMostTaggedUsernames() {
 		List<String> list = tagForUserDao.getMostTagged();
 		return list;
 	}
-	
+
 	public List<DatatypeMostTagged> getMostTagged(int length) {
 		List<String> list = getMostTaggedUsernames();
 		if (list.size() > length) {
@@ -288,46 +329,50 @@ public class ServicesUser implements ServicesUserRemote {
 		return null;
 	}
 
-	public List<DatatypeNotification> getUpdateNotificationsReceived(String username) {
-    	NormalUser nUser = getNormalUser(username);
-    	List<Notification> notif = nUser.getNotificationsReceived();
-    	return TranslatorCollection.translateNotification(notif);
+	public List<DatatypeNotification> getUpdateNotificationsReceived(
+			String username) {
+		NormalUser nUser = getNormalUser(username);
+		List<Notification> notif = nUser.getNotificationsReceived();
+		return TranslatorCollection.translateNotification(notif);
 	}
 
 	public List<DatatypeNotification> getUpdateNotificationsSent(String username) {
-    	NormalUser nUser = getNormalUser(username);
-    	List<Notification> notif = nUser.getNotificationsCreated();
-    	return TranslatorCollection.translateNotification(notif);
+		NormalUser nUser = getNormalUser(username);
+		List<Notification> notif = nUser.getNotificationsCreated();
+		return TranslatorCollection.translateNotification(notif);
 	}
-	
-	public DatatypeNotification createNotification(String fromUser, String toUser, 
-			Integer type, String message, String subject) throws UserNotFoundException, MessageTooLongException{
-		
+
+	public DatatypeNotification createNotification(String fromUser,
+			String toUser, Integer type, String message, String subject)
+			throws UserNotFoundException, MessageTooLongException {
+
 		User fUser;
 		User tUser;
-		
-		if(subject != null && subject.length() > 200)
-			throw new MessageTooLongException("El asunto del mensaje debe tener menos de 200 caracteres.");
-		
-		if(message.length() > 5000)
-			throw new MessageTooLongException("El mensaje debe tener menos de 5000 caracteres.");
-		
-		if(existsNormalUser(fromUser)) {
+
+		if (subject != null && subject.length() > 200)
+			throw new MessageTooLongException(
+					"El asunto del mensaje debe tener menos de 200 caracteres.");
+
+		if (message.length() > 5000)
+			throw new MessageTooLongException(
+					"El mensaje debe tener menos de 5000 caracteres.");
+
+		if (existsNormalUser(fromUser)) {
 			fUser = nUserDao.findByID(fromUser);
-		} else if(existsAdminUser(fromUser)) {
+		} else if (existsAdminUser(fromUser)) {
 			fUser = adminDao.findByID(fromUser);
 		} else {
 			throw new UserNotFoundException("No existe el usuario " + fromUser);
 		}
-		
-		if(existsNormalUser(toUser)) {
+
+		if (existsNormalUser(toUser)) {
 			tUser = nUserDao.findByID(toUser);
-		} else if(existsAdminUser(toUser)) {
+		} else if (existsAdminUser(toUser)) {
 			tUser = adminDao.findByID(toUser);
 		} else {
 			throw new UserNotFoundException("No existe el usuario " + toUser);
 		}
-		
+
 		Notification not = new Notification();
 		not.setNotDate(new Timestamp((new java.util.Date()).getTime()));
 		not.setRead(false);
@@ -337,48 +382,54 @@ public class ServicesUser implements ServicesUserRemote {
 		not.setUserFrom(fUser);
 		not.setUserTo(tUser);
 		not.setSubject(subject);
-		
+
 		notifDao.persist(not);
-		
-		//necetiso el flush para que falle persistir notification y no se mande mail en ese caso
+
+		// necetiso el flush para que falle persistir notification y no se mande
+		// mail en ese caso
 		notifDao.flush();
-		
-		return (DatatypeNotification)new TranslatorNotification().translate(not);
+
+		return (DatatypeNotification) new TranslatorNotification()
+				.translate(not);
 	}
 
-	public DatatypeUser updateUserAvatar(String username, byte[] data, String mime)
-			throws UserNotFoundException {
-		
-		if(!existsNormalUser(username))
+	public DatatypeUser updateUserAvatar(String username, byte[] data,
+			String mime) throws UserNotFoundException {
+
+		if (!existsNormalUser(username))
 			throw new UserNotFoundException();
-		
+
 		NormalUser nu = nUserDao.findByID(username);
-		String imgPath = fileSystem.writeFile(data, mime, AVATAR_PATH + "/" + username + "/");
+		String imgPath = fileSystem.writeFile(data, mime, AVATAR_PATH + "/"
+				+ username + "/");
 		String oldImage = nu.getImgPath();
-		if(oldImage != null && !oldImage.equals("") && !oldImage.equals(DEFAULT_IMAGE))
+		if (oldImage != null && !oldImage.equals("")
+				&& !oldImage.equals(DEFAULT_IMAGE))
 			fileSystem.deleteFile(oldImage);
 		nu.setImgPath(imgPath);
 		nUserDao.persist(nu);
-		return (DatatypeUser)new TranslatorUser().translate(nu);
+		return (DatatypeUser) new TranslatorUser().translate(nu);
 	}
 
 	public boolean isNormalUserPassword(String username, String password) {
 		NormalUser user = getNormalUser(username);
 		try {
 			return user.getPassword().equals(CryptUtils.encript(password));
-		} catch(NoSuchAlgorithmException e) {
-			System.out.println("Error en la autenticación del usuario " + username + " para la contraseña " + password);
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error en la autenticaciï¿½n del usuario "
+					+ username + " para la contraseï¿½a " + password);
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
+
 	public boolean isAdminPassword(String username, String password) {
 		Admin user = getAdminUser(username);
 		try {
 			return user.getPassword().equals(CryptUtils.encript(password));
-		} catch(NoSuchAlgorithmException e) {
-			System.out.println("Error en la autenticación del usuario " + username + " para la contraseña " + password);
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error en la autenticaciï¿½n del usuario "
+					+ username + " para la contraseï¿½a " + password);
 			e.printStackTrace();
 			return false;
 		}
