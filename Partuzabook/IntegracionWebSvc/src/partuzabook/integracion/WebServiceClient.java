8702a3d473e8ba162bc4c3afda52ec28e73454ae
jefa55@yahoo.com <jefa55@yahoo.com@eaa6a7b1-3fd6-b1f1-36e1-1d5117f4dc80>
2010-11-20 15:30:28 +0000

    Busqueda completa

--- Partuzabook/IntegracionWebSvc/src/partuzabook/integracion/WebServiceClient.java
+++ Partuzabook/IntegracionWebSvc/src/partuzabook/integracion/WebServiceClient.java
@@ -4,9 +4,12 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Holder;
 import javax.xml.ws.Service;
+
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 
 import partuzabook.integracion.ws.Busqueda;
 import partuzabook.integracion.ws.BusquedaService;
@@ -30,8 +33,35 @@ public class WebServiceClient {
 			Holder<Integer> tot = new Holder<Integer>();
 			tot.value = new Integer(0);
 			busqueda.searchByName("", 100, c, tot);
+			System.out.println("TOTAL ES:::  " + tot.value);
+			List<Evento> result = c.value;
+			for (Evento evento : result) {
+				System.out.println("Evento encontrado:::  " + evento.getNombre());
+			}
 			
-			System.out.println("DESCUENTO ES:::  " + tot.value);
+			c.value = new ArrayList<Evento>();
+			tot.value = new Integer(0);
+			XMLGregorianCalendarImpl fechaFrom = new XMLGregorianCalendarImpl();
+			fechaFrom.setYear(2011);
+			fechaFrom.setMonth(10);
+			fechaFrom.setDay(1);
+			fechaFrom.setHour(0);
+			fechaFrom.setMinute(0);
+			fechaFrom.setSecond(0);
+			
+			XMLGregorianCalendarImpl fechaTo = new XMLGregorianCalendarImpl();
+			fechaTo.setYear(2011);
+			fechaTo.setMonth(10);
+			fechaTo.setDay(31);
+			fechaTo.setHour(0);
+			fechaTo.setMinute(0);
+			fechaTo.setSecond(0);
+			busqueda.searchBetweenDate(fechaFrom, fechaTo, 100, c, tot);
+			System.out.println("TOTAL ES:::  " + tot.value);
+			result = c.value;
+			for (Evento evento : result) {
+				System.out.println("Evento encontrado:::  " + evento.getNombre());
+			}
 
 			// do something with the service stub here...
 		} catch (Exception e) {
--- Partuzabook/IntegracionWebSvc/src/partuzabook/integracion/ws/ObjectFactory.java
+++ Partuzabook/IntegracionWebSvc/src/partuzabook/integracion/ws/ObjectFactory.java
@@ -25,7 +25,11 @@ import javax.xml.namespace.QName;
 public class ObjectFactory {
 
     private final static QName _SearchByName_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchByName");
+    private final static QName _SearchByDateResponse_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchByDateResponse");
+    private final static QName _SearchBetweenDate_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchBetweenDate");
+    private final static QName _SearchBetweenDateResponse_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchBetweenDateResponse");
     private final static QName _SearchByNameResponse_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchByNameResponse");
+    private final static QName _SearchByDate_QNAME = new QName("http://edu.tsi2.ws/integracion/ws/busqueda", "searchByDate");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: partuzabook.integracion.ws
@@ -35,19 +39,27 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link SearchRequest }
+     * Create an instance of {@link SearchBetweenDateRequest }
      * 
      */
-    public SearchRequest createSearchRequest() {
-        return new SearchRequest();
+    public SearchBetweenDateRequest createSearchBetweenDateRequest() {
+        return new SearchBetweenDateRequest();
     }
 
     /**
-     * Create an instance of {@link Evento }
+     * Create an instance of {@link SearchByNameRequest }
      * 
      */
-    public Evento createEvento() {
-        return new Evento();
+    public SearchByNameRequest createSearchByNameRequest() {
+        return new SearchByNameRequest();
+    }
+
+    /**
+     * Create an instance of {@link SearchByDateRequest }
+     * 
+     */
+    public SearchByDateRequest createSearchByDateRequest() {
+        return new SearchByDateRequest();
     }
 
     /**
@@ -59,12 +71,47 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link SearchRequest }{@code >}}
+     * Create an instance of {@link Evento }
+     * 
+     */
+    public Evento createEvento() {
+        return new Evento();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SearchByNameRequest }{@code >}}
      * 
      */
     @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchByName")
-    public JAXBElement<SearchRequest> createSearchByName(SearchRequest value) {
-        return new JAXBElement<SearchRequest>(_SearchByName_QNAME, SearchRequest.class, null, value);
+    public JAXBElement<SearchByNameRequest> createSearchByName(SearchByNameRequest value) {
+        return new JAXBElement<SearchByNameRequest>(_SearchByName_QNAME, SearchByNameRequest.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SearchResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchByDateResponse")
+    public JAXBElement<SearchResponse> createSearchByDateResponse(SearchResponse value) {
+        return new JAXBElement<SearchResponse>(_SearchByDateResponse_QNAME, SearchResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SearchBetweenDateRequest }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchBetweenDate")
+    public JAXBElement<SearchBetweenDateRequest> createSearchBetweenDate(SearchBetweenDateRequest value) {
+        return new JAXBElement<SearchBetweenDateRequest>(_SearchBetweenDate_QNAME, SearchBetweenDateRequest.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SearchResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchBetweenDateResponse")
+    public JAXBElement<SearchResponse> createSearchBetweenDateResponse(SearchResponse value) {
+        return new JAXBElement<SearchResponse>(_SearchBetweenDateResponse_QNAME, SearchResponse.class, null, value);
     }
 
     /**
@@ -74,6 +121,15 @@ public class ObjectFactory {
     @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchByNameResponse")
     public JAXBElement<SearchResponse> createSearchByNameResponse(SearchResponse value) {
         return new JAXBElement<SearchResponse>(_SearchByNameResponse_QNAME, SearchResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SearchByDateRequest }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://edu.tsi2.ws/integracion/ws/busqueda", name = "searchByDate")
+    public JAXBElement<SearchByDateRequest> createSearchByDate(SearchByDateRequest value) {
+        return new JAXBElement<SearchByDateRequest>(_SearchByDate_QNAME, SearchByDateRequest.class, null, value);
     }
 
 }
