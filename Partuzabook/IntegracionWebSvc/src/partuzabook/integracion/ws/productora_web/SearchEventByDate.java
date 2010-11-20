8bba3227e52d10b6573f153d2e96857e656eaaa7
jefa55@yahoo.com <jefa55@yahoo.com@eaa6a7b1-3fd6-b1f1-36e1-1d5117f4dc80>
2010-11-20 20:15:45 +0000

    integracion con ProductoraWeb. Wrappers y tipos para el uso de su servicio web.

---
+++ Partuzabook/IntegracionWebSvc/src/partuzabook/integracion/ws/productora_web/SearchEventByDate.java
@@ -0,0 +1,66 @@
+
+package partuzabook.integracion.ws.productora_web;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="date" type="{http://www.w3.org/2001/XMLSchema}date" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "date"
+})
+@XmlRootElement(name = "searchEventByDate")
+public class SearchEventByDate {
+
+    @XmlElementRef(name = "date", namespace = "http://ejb.integracion.tsi2.fing.edu.uy", type = JAXBElement.class)
+    protected JAXBElement<XMLGregorianCalendar> date;
+
+    /**
+     * Gets the value of the date property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link XMLGregorianCalendar }{@code >}
+     *     
+     */
+    public JAXBElement<XMLGregorianCalendar> getDate() {
+        return date;
+    }
+
+    /**
+     * Sets the value of the date property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link XMLGregorianCalendar }{@code >}
+     *     
+     */
+    public void setDate(JAXBElement<XMLGregorianCalendar> value) {
+        this.date = ((JAXBElement<XMLGregorianCalendar> ) value);
+    }
+
+}
